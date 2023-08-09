663d7c16cfc7e01be1c2b585cbb63ea12128382e
magic-akari <akari.ccino@gmail.com>
2023-08-09 23:29:39 +0800

    refactor: split test

--- crates/ruff_fmt/src/lib.rs
+++ crates/ruff_fmt/src/lib.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod test;
+
 use ruff_python_formatter::format_module;
 use wasm_bindgen::prelude::*;
 
@@ -8,48 +11,3 @@ pub fn format(input: &str) -> Result<String, String> {
         .map_err(|err| err.to_string())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::format;
-
-    #[test]
-    fn it_works() {
-        let input = r###"x = {  'a':37,'b':42,
-
-'c':927}
-
-y = 'hello ''world'
-z = 'hello '+'world'
-a = 'hello {}'.format('world')
-class foo  (     object  ):
-  def f    (self   ):
-    return       37*-+2
-  def g(self, x,y=42):
-      return y
-def f  (   a ) :
-  return      37+-+a[42-x :  y**3]"###;
-
-        let expected = r###"x = {"a": 37, "b": 42, "c": 927}
-
-y = "hello " "world"
-z = "hello " + "world"
-a = "hello {}".format("world")
-
-
-class foo(object):
-    def f(self):
-        return 37 * -+2
-
-    def g(self, x, y=42):
-        return y
-
-
-def f(a):
-    return 37 + -+a[42 - x : y**3]
-"###;
-
-        let actual = format(input).unwrap();
-
-        assert_eq!(actual, expected);
-    }
-}
---
+++ crates/ruff_fmt/src/test.rs
@@ -0,0 +1,23 @@
+#[cfg(test)]
+mod tests {
+    use std::{fs::File, io::Read, path::PathBuf};
+    use testing_macros::fixture;
+
+    use crate::format;
+
+    #[fixture("test_data/*.input")]
+    fn it_works(input: PathBuf) {
+        // calc the expected file path
+        let extect_path = input.with_extension("expect");
+
+        let mut actual = String::new();
+        File::open(&input).and_then(|mut file| file.read_to_string(&mut actual)).unwrap();
+
+        let mut expect = String::new();
+        File::open(&extect_path).and_then(|mut file| file.read_to_string(&mut expect)).unwrap();
+
+        let actual = format(&actual).unwrap();
+
+        assert_eq!(actual, expect);
+    }
+}
