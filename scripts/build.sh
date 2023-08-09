b396fd1539eed7c734ac2e207c9716c469b5ad10
magic-akari <akari.ccino@gmail.com>
2023-08-09 15:35:55 +0800

    fix: npm pack

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@ cp ./pkg/ruff_fmt.js ./pkg/ruff_fmt.js.bak
 
 git apply ./scripts/ruff_fmt.patch
 
-cp ./scripts/vite.js ./pkg/
+cp ./scripts/vite.js ./scripts/.npmignore ./pkg/
 
 ./scripts/package.mjs ./pkg/package.json
