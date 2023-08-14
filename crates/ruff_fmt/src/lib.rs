03b5e3b8d951dfa5b1df4c22265ea2775b226c46
magic-akari <akari.ccino@gmail.com>
2023-08-14 14:13:25 +0800

    feat: support format config

--- crates/ruff_fmt/src/lib.rs
+++ crates/ruff_fmt/src/lib.rs
@@ -1,12 +1,31 @@
+mod config;
 #[cfg(test)]
 mod test;
 
+use config::Config as InnerConfig;
 use ruff_python_formatter::format_module;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn format(input: &str) -> Result<String, String> {
     format_module(input, Default::default())
-        .map(|result| result.as_code().to_string())
+        .map(|result| result.into_code())
         .map_err(|err| err.to_string())
+}
+
+#[wasm_bindgen]
+pub fn format_with_config(input: &str, config: &Config) -> Result<String, String> {
+    let config: &JsValue = config.as_ref();
+    let config: InnerConfig =
+        serde_wasm_bindgen::from_value(config.clone()).map_err(|op| op.to_string())?;
+
+    format_module(input, config.into())
+        .map(|result| result.into_code())
+        .map_err(|err| err.to_string())
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(typescript_type = "Config")]
+    pub type Config;
 }
