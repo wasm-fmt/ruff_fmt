b1ac4c8643defb88ea2c2913bfe3907d9e9cbfbc
magic-akari <akari.ccino@gmail.com>
2023-08-14 11:28:53 +0800

    fix: build script

--- crates/ruff_fmt/scripts/build.sh
+++ crates/ruff_fmt/scripts/build.sh
@@ -1,12 +1,15 @@
+cd $(dirname $0)/..
+crates_dir=$(pwd)
+
+cd ../..
 wasm-pack build --target=web --scope=wasm-fmt crates/ruff_fmt
 
-cd crates/ruff_fmt
-
+cd $crates_dir
 # backup ruff_fmt.js
 cp ./pkg/ruff_fmt.js ./pkg/ruff_fmt.js.bak
 
 git apply ./patch/ruff_fmt.patch
 
-cp -r ./extra/ ./pkg/
+cp -R ./extra/. ./pkg/
 
 ./scripts/package.mjs ./pkg/package.json
