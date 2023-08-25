c527f1d7af1191fdd73b4192b17c8f179af82ee8
magic-akari <akari.ccino@gmail.com>
2023-08-26 02:07:54 +0800

    chore: update ci scripts

--- crates/ruff_fmt/scripts/package.mjs
+++ crates/ruff_fmt/scripts/package.mjs
@@ -9,21 +9,22 @@ const pkg_json = JSON.parse(pkg_text);
 
 delete pkg_json.files;
 
+pkg_json.main = pkg_json.module;
 pkg_json.type = "module";
 pkg_json.publishConfig = {
-  access: "public",
+    access: "public",
 };
 pkg_json.exports = {
-  ".": {
-    types: "./ruff_fmt.d.ts",
-    default: "./ruff_fmt.js",
-  },
-  "./vite": {
-    types: "./ruff_fmt.d.ts",
-    default: "./vite.js",
-  },
-  "./package.json": "./package.json",
-  "./*": "./*",
+    ".": {
+        types: "./ruff_fmt.d.ts",
+        default: "./ruff_fmt.js",
+    },
+    "./vite": {
+        types: "./ruff_fmt.d.ts",
+        default: "./vite.js",
+    },
+    "./package.json": "./package.json",
+    "./*": "./*",
 };
 
 fs.writeFileSync(pkg_path, JSON.stringify(pkg_json, null, 4));
