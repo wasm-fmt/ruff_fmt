ce767692d903deb4862f3c3cac2da16b76b5bc22
magic-akari <akari.ccino@gmail.com>
2024-10-26 10:54:58 +0800

    fix: jsr publish issue

--- crates/ruff_fmt/scripts/build.sh
+++ crates/ruff_fmt/scripts/build.sh
@@ -6,8 +6,6 @@ wasm-pack build --target=web --scope=wasm-fmt crates/ruff_fmt
 cp README.md crates/ruff_fmt/pkg/
 
 cd $crates_dir
-# backup ruff_fmt.js
-cp ./pkg/ruff_fmt.js ./pkg/ruff_fmt.js.bak
 
 cp -R ./extra/. ./pkg/
 
